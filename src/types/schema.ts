2e3cdeec52fedf135230ccec46f3ef62fb94a7a2
bryantgillespie <hey@bryantgillespie.com>
2025-05-01 19:09:26 -0400

    cleanup

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,6 +1,7 @@
 export type CollectionName = string;
 export type FieldName = string;
 export type Schema = Record<CollectionName, Record<FieldName, Field>>;
+
 export interface Field {
 	type: string | null;
 	interface?: string | null | undefined;
@@ -8,7 +9,6 @@ export interface Field {
 	primary_key?: boolean | null | undefined;
 	required?: boolean | null | undefined;
 	choices?: Record<string, string>[] | null | undefined;
-
 	// Relationship details
 	relation_type?: 'm2o' | 'o2m' | 'm2m' | 'm2a' | 'file' | 'files' | null | undefined;
 	relation_collection?: string | string[] | null | undefined; // Collection(s) on the other side
